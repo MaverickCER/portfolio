4c5d5284eb0fa201a40b646cfe4dd16d13bdf99d
Maverick Manasco <MaverickCER@gmail.com>
2021-11-26 13:08:12 -0500

    Initial Commit
    Modified portfolio template to better suit the need of this project and personal goals.

---
+++ README.md
@@ -0,0 +1,106 @@
+<!--
+*** Thanks for checking out the portfolio README.md
+*** If would like to help make this design better,
+*** please fork the repo and create a pull request.
+-->
+# Portfolio for MaverickCER
+<!-- MaverickCER LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/MaverickCER/portfolio/edit/main/README.md">
+    <img src="https://i.imgur.com/FPLubfR.png" alt="Logo" width="80" height="auto">
+  </a>
+
+  <h3 align="center">Portfolio README</h3>
+
+  <p align="center">
+    A web developer's portfolio.
+    <br />
+    <a href="https://github.com/MaverickCER/portfolio/img/websitethumb.png"><strong>Explore the development code »</strong></a>
+    <br />
+    <br />
+    <a href="https://www.maverickcer.dev">View Live Portfolio</a>
+    ·
+    <a href="https://github.com/MaverickCER/portfolio/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/MaverickCER/portfolio/issues">Request Feature</a>
+  </p>
+</p>
+
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul><li><a href="#built-with">Built With</a></li></ul>
+    </li>
+    <li><a href="#getting-started">Getting Started</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgements">Acknowledgements</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+![Screenshot](https://github.com/MaverickCER/portfolio/img/websitethumb.png "Portfolio")
+
+This is my personal portfolio built with the goal of obtaining a full time position.
+The development code can be found at [https://github.com/MaverickCER/portfolio](https://github.com/MaverickCER/portfolio)<br/>
+The live site is available at [https://www.maverickcer.dev](https://www.maverickcer.dev)
+
+### Built With
+
+* [HTML5](https://reactjs.org/)
+* [EmailJS](https://www.emailjs.com/)
+* [CodeSandbox](https://www.codesandbox.io/)
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Install [nodejs](https://nodejs.org/)<br/>
+Clone the react [repo](https://github.com/MaverickCER/portfolio)<br/>
+In your console, navigate to the repo folder and run `npm i`<br/>
+I utilize firebase, trigger email, and mailgun to set up free email confirmations.
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+See [Issues](https://github.com/MaverickCER/portfolio/issues) for a list of proposed features (and known issues).
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+As this is a portfolio site, I am not accepting any contributions.<br/>
+Feel free to clone and improve the site for your own use!<br/>
+If you have suggestions, they can be added to [Issues](https://github.com/MaverickCER/portfolio/issues)
+
+
+<!-- CONTACT -->
+## Contact
+
+Maverick Manasco - @MaverickCER<br/>
+[<img src="https://i.imgur.com/ug3iVUk.png" alt="https://www.maverickcer.dev" width="auto" height="24px">](https://www.maverickcer.dev) 
+[![maverickcer@gmail.com](https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/Email-24.png)](mailto:maverickcer@gmail.com) 
+[![CodePen](https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/Codepen-24.png)](https://codepen.io/maverickcer) 
+[<img src="https://i.imgur.com/QAFF9m4.png" alt="CodeSandbox" width="auto" height="24px">](https://codesandbox.io/u/MaverickCER) 
+[![GitHub](https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/GitHub-24.png)](https://github.com/MaverickCER) 
+[![LinkedIn](https://cdn3.iconfinder.com/data/icons/social-rounded-2/72/Linkedin-24.png)](https://www.linkedin.com/in/maverickcer/) 
+[![Reddit](https://cdn0.iconfinder.com/data/icons/social-rounded/72/Reddit-24.png)](https://www.reddit.com/user/maverickcer) 
+[![Twitter](https://cdn0.iconfinder.com/data/icons/social-rounded/72/Twitter-24.png)](https://twitter.com/MaverickCER) 
+[![YouTube](https://cdn0.iconfinder.com/data/icons/social-rounded/72/Youtube-24.png)](https://studio.youtube.com/channel/UCkYSvi4dRFcsrSIbE5Sflmg)
+
+Project Link: [https://github.com/MaverickCER/portfolio](https://github.com/MaverickCER/portfolio)
+
+
+<!-- ACKNOWLEDGEMENTS -->
+## Acknowledgements
+* [GitHub Pages](https://pages.github.com)
---
+++ assets/js/main.js
@@ -0,0 +1,157 @@
+if (navigator.serviceWorker) {
+	navigator.serviceWorker
+		.register('/sw.js')
+		.then(function(registration) {
+			// console.log(
+			// 	'ServiceWorker registration successful with scope:',
+			// 	registration.scope
+			// );
+		})
+		.catch(function(error) {
+			// console.log('ServiceWorker registration failed:', error);
+		});
+}
+// -----------------------------------------------------
+// ---------------   PRELOADER   -----------------------
+// -----------------------------------------------------
+setTimeout(function() {
+	if (
+		/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+			navigator.userAgent
+		)
+	) {
+		setTimeout(function() {
+			document.getElementById('preloader').classList.add('preloaded');
+		}, 800);
+		setTimeout(function() {
+			document.getElementById('preloader').remove();
+		}, 2000);
+	} else {
+		setTimeout(function() {
+			document.getElementById('preloader').classList.add('preloaded');
+		}, 500);
+		setTimeout(function() {
+			document.getElementById('preloader').remove();
+			document
+				.getElementsByClassName('beny_tm_all_wrap')[0]
+				.classList.add('animate');
+		}, 1500);
+	}
+}, 500);
+// -----------------------------------------------------
+// -----------------   Carousel    ---------------------
+// -----------------------------------------------------
+var glide = new Glide('.glide', {
+	type: 'carousel',
+	animationDuration: 500,
+	autoplay: false,
+	focusAt: '2',
+	startAt: 2,
+	perView: 1
+});
+window.onload = event => {
+	glide.mount();
+	// -----------------------------------------------------
+	// -----------------   Contact    ----------------------
+	// -----------------------------------------------------
+	const firebaseConfig = {
+		apiKey: 'AIzaSyC8C5_WNvOWNs46wcVJiBwPLbcewJOGNfU',
+		authDomain: 'callouts-evolved.firebaseapp.com',
+		projectId: 'callouts-evolved',
+		storageBucket: 'callouts-evolved.appspot.com',
+		messagingSenderId: '682142902161',
+		appId: '1:682142902161:web:bdb6868dbb2a4b53769c30',
+		measurementId: 'G-PBF9ZRCMEX'
+	};
+	const app = firebase.initializeApp(firebaseConfig);
+	const db = firebase.firestore();
+	document.getElementById('contact-form').addEventListener(
+		'submit',
+		function(e) {
+			e.preventDefault();
+			contact(db);
+			return false;
+		},
+		false
+	);
+	// -----------------------------------------------------
+	// ---------------   Mobile Menu    --------------------
+	// -----------------------------------------------------
+	document.getElementsByClassName('my_trigger')[0].addEventListener(
+		'click',
+		function(e) {
+			document
+				.getElementsByClassName('hamburger')[0]
+				.classList.toggle('is-active');
+			document
+				.getElementsByClassName('mobile_menu_dropdown')[0]
+				.classList.toggle('is-active');
+		},
+		false
+	);
+
+	let mobile_links = document.getElementsByClassName('mobile_link');
+	for (var i = 0; i < mobile_links.length; i++) {
+		(index => {
+			mobile_links[index].addEventListener('click', () => {
+				document
+					.getElementsByClassName('hamburger')[0]
+					.className.remove('is-active');
+				document
+					.getElementsByClassName('my_trigger')[0]
+					.classList.remove('is-active');
+			});
+		})(i);
+	}
+	// -----------------------------------------------------
+	// ----------------   Scroll Spy    --------------------
+	// -----------------------------------------------------
+	const sections = document.querySelectorAll('section[id]');
+	function scrollActive() {
+		const scrollY = window.pageYOffset;
+		sections.forEach(current => {
+			const sectionHeight = current.offsetHeight;
+			const sectionTop = current.offsetTop - 50;
+			sectionId = current.getAttribute('id');
+			if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+				document
+					.querySelector('.anchor_nav a[href*=' + sectionId + ']')
+					.classList.add('current');
+			} else {
+				document
+					.querySelector('.anchor_nav a[href*=' + sectionId + ']')
+					.classList.remove('current');
+			}
+		});
+	}
+	window.addEventListener('scroll', scrollActive);
+	// -------------------------------------------------
+	// -------------  MODALBOX SERVICE -----------------
+	// -------------------------------------------------
+	var modalBox = document.getElementsByClassName(
+		'beny_tm_modalbox_portfolio'
+	)[0];
+	var list = document.getElementsByClassName('portfolio_list_item');
+	var closePopup = document.getElementsByClassName('close_modalbox')[0];
+	for (var i = 0; i < list.length; i++) {
+		(index => {
+			list[index].addEventListener('click', () => {
+				document.getElementsByClassName('description_wrap')[0].innerHTML = list[
+					index
+				].querySelector('.popup_informations').innerHTML;
+				document
+					.getElementsByClassName('beny_tm_modalbox_portfolio')[0]
+					.classList.add('opened');
+			});
+		})(i);
+	}
+	document
+		.getElementsByClassName('close_modalbox')[0]
+		.addEventListener('click', () => {
+			document
+				.getElementsByClassName('beny_tm_modalbox_portfolio')[0]
+				.classList.remove('opened');
+			document.getElementsByClassName('description_wrap')[0].innerHTML = '';
+			return false;
+		});
+};
